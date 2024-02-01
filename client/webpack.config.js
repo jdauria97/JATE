5d1ecfbcdb6815beeb683b49768e07ec6673d30d
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-01-31 19:30:58 -0500

    configures webpacks

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "TODOs List",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Text Editor",
+        short_name: "JATE",
+        description: "Text Editor for Browser",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "./",
+
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
